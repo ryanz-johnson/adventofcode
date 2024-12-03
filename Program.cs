0ba2fdf9f2cd5cc763c4338518d583a3967ae8df
Ryan Johnson (IoT) <johnsonryan@microsoft.com>
2024-12-03 13:43:03 -0800

    just add a new class

--- Program.cs
+++ Program.cs
@@ -8,13 +8,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        var day1 = new Day1();
-        day1.Solve();
+        var program = new Program();
+    }
 
-        var day2 = new Day2();
-        day2.Solve();
-
-        var day3 = new Day3();
-        day3.Solve();
+    public Program()
+    {
+        int now = DateTime.Now.Day;
+        for (int i = 1; i <= now; i++)
+        {
+            var dayType = Type.GetType($"Day{i}");
+            if (dayType != null)
+            {
+                var dayInstance = Activator.CreateInstance(dayType);
+                var solveMethod = dayType.GetMethod("Solve");
+                solveMethod?.Invoke(dayInstance, null);
+            }
+        }
     }
 }
